60b9c25d894dac5aa3bbbf0559019e9bf7fa1498
juancgalueweb <juancgalue@icloud.com>
2023-12-04 14:41:54 -0300

    Texto agregado a reto 4

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 > Solo para practicar. El código está hecho con Javascript y las pruebas con Vitest.
 
 ## Página web de los retos de AdventJS
+
 https://adventjs.dev/es
 
 ## Página web de los retgos de Advent Of Code
+
 https://adventofcode.com/2023
--- day4/adventjs_4.js
+++ day4/adventjs_4.js
@@ -1,3 +1,15 @@
+/* 
+  En el taller de Santa 🎅, algunos mensajes navideños han sido escritos de manera peculiar: 
+  las letras dentro de los paréntesis deben ser leídas al revés
+
+  Santa necesita que estos mensajes estén correctamente formateados. 
+  Tu tarea es escribir una función que tome una cadena de texto y revierta los caracteres 
+  dentro de cada par de paréntesis, eliminando los paréntesis en el mensaje final.
+
+  Eso sí, ten en cuenta que pueden existir paréntesis anidados, por lo que debes invertir 
+  los caracteres en el orden correcto.
+*/
+
 export function decode(message) {
   const countOpenAndCloseParenthesis = message.match(/\(|\)/g)
   let parenthesisCounter =
