8620e3403c87f650b5c7906adcf07b1db545af39
juancgalueweb <juancgalue@icloud.com>
2023-12-07 22:54:52 -0300

    Advent of code, day 3, part 2

--- day3/adventofcode_3.js
+++ day3/adventofcode_3.js
@@ -1,50 +1,9 @@
-/*
-  --- Day 3: Gear Ratios ---
-You and the Elf eventually reach a gondola lift station; 
-he says the gondola lift will take you up to the water source, 
-but this is as far as he can bring you. You go inside.
-
-It doesn't take long to find the gondolas, but there seems to be a problem: 
-they're not moving.
-
-"Aaah!"
-
-You turn around to see a slightly-greasy Elf with a wrench and a look of surprise. 
-"Sorry, I wasn't expecting anyone! The gondola lift isn't working right now; 
-it'll still be a while before I can fix it." You offer to help.
-
-The engineer explains that an engine part seems to be missing from the engine, 
-but nobody can figure out which one. If you can add up all the part numbers in the engine schematic, 
-it should be easy to work out which part is missing.
-
-The engine schematic (your puzzle input) consists of a visual representation of the engine. 
-There are lots of numbers and symbols you don't really understand, 
-but apparently any number adjacent to a symbol, even diagonally, is a "part number" 
-and should be included in your sum. (Periods (.) do not count as a symbol.)
-
-Here is an example engine schematic:
-
-467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..
-In this schematic, two numbers are not part numbers because they are not adjacent to a symbol: 114 (top right) and 58 (middle right). Every other number is adjacent to a symbol and so is a part number; their sum is 4361.
-
-Of course, the actual engine schematic is much larger. What is the sum of all of the part numbers in the engine schematic?
-*/
-
 import fs from 'node:fs'
 
 const engineSchematicTest = fs
   .readFileSync('../input/adventofcode_day3_1_short.txt', 'utf8')
   .split('\n')
-  .map((row) => row.split(''))
+  .filter((n) => n)
 
 function part1(matrix) {
   const size = matrix.length
@@ -87,103 +46,64 @@ function part1(matrix) {
 
 console.log(part1(engineSchematicTest))
 
-/* 
---- Part Two ---
-The engineer finds the missing part and installs it in the engine! As the engine springs 
-to life, you jump in the closest gondola, finally ready to ascend to the water source.
-
-You don't seem to be going very fast, though. Maybe something is still wrong? 
-Fortunately, the gondola has a phone labeled "help", so you pick it up and the engineer answers.
-
-Before you can explain the situation, she suggests that you look out the window. 
-There stands the engineer, holding a phone in one hand and waving with the other. 
-You're going so slowly that you haven't even left the station. You exit the gondola.
-
-The missing part wasn't the only issue - one of the gears in the engine is wrong. 
-A gear is any * symbol that is adjacent to exactly two part numbers. 
-Its gear ratio is the result of multiplying those two numbers together.
-
-This time, you need to find the gear ratio of every gear and add them all up 
-so that the engineer can figure out which gear needs to be replaced.
-
-Consider the same engine schematic again:
-
-467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..
-In this schematic, there are two gears. The first is in the top left; 
-it has part numbers 467 and 35, so its gear ratio is 16345. The second gear is in the lower right; 
-its gear ratio is 451490. 
-(The * adjacent to 617 is not a gear because it is only adjacent to one part number.) 
-Adding up all of the gear ratios produces 467835.
-
-What is the sum of all of the gear ratios in your engine schematic?
-*/
-
 function part2(matrix) {
-  const size = matrix.length
-  const asthericCoordinates = []
+  const rows = matrix.length
+  const cols = matrix[0].length
+  const gearsObj = {}
 
-  for (let row = 0; row < size; row++) {
-    for (let col = 0; col < size; col++) {
-      if (matrix[row][col] === '*') {
-        asthericCoordinates.push([row, col])
+  const findGears = (str, num, i, j) => {
+    j = j === -1 ? 0 : j
+    for (let k = 0; k < str.length; k++) {
+      const character = str.charAt(k)
+      if (character === '*') {
+        const index = `${i}-${j + k}`
+        gearsObj[index] = gearsObj[index]
+          ? [...gearsObj[index], parseInt(num)]
+          : [parseInt(num)]
       }
     }
   }
 
-  return asthericCoordinates
-}
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      const current = matrix[i][j]
 
-function findAdyacentsNumbers(matrix, coordinatesArray) {
-  const size = matrix.length
-  const results = []
-  let adyacentsNumbers = []
-  let numbersCoordinates = []
+      if (isNaN(current)) {
+        continue
+      }
 
-  coordinatesArray.forEach((coordinates) => {
-    const row = coordinates[0]
-    const col = coordinates[1]
+      let num = current
 
-    for (let i = -1; i <= 1; i++) {
-      for (let j = -1; j <= 1; j++) {
-        const newRow = row + i
-        const newCol = col + j
-
-        if (
-          newRow >= 0 &&
-          newRow < size &&
-          newCol >= 0 &&
-          newCol < size &&
-          !(i === 0 && j === 0)
-        ) {
-          if (!isNaN(parseInt(matrix[newRow][newCol]))) {
-            adyacentsNumbers.push(matrix[newRow][newCol])
-            numbersCoordinates.push([newRow, newCol])
-          }
+      // If used prefix, with operator before operand (for example, ++x),
+      // the increment operator increments and returns the value after incrementing.
+      while (++j < cols) {
+        if (Number.isInteger(parseInt(matrix[i][j]))) {
+          num += matrix[i][j]
+        } else {
+          break
         }
       }
-    }
-    if (adyacentsNumbers.length > 1) {
-      results.push({ adyacentsNumbers, numbersCoordinates })
-    }
-    adyacentsNumbers = []
-    numbersCoordinates = []
-  })
 
-  return results
+      const top =
+        i === 0 ? '' : matrix[i - 1].substring(j - num.length - 1, j + 1)
+      const bottom =
+        i === rows - 1 ? '' : matrix[i + 1].substring(j - num.length - 1, j + 1)
+      const left = matrix[i][j - num.length - 1] || ''
+      const right = matrix[i][j] || ''
+
+      // console.log(num, i, j, top, right, bottom, left)
+
+      findGears(top, num, i - 1, j - num.length - 1)
+      findGears(bottom, num, i + 1, j - num.length - 1)
+      findGears(left, num, i, j - num.length - 1)
+      findGears(right, num, i, j)
+    }
+  }
+  return Object.values(gearsObj)
+    .filter((arr) => arr.length === 2)
+    .reduce((acc, cur) => acc + cur[0] * cur[1], 0)
 }
 
-const asthericCoordinates = part2(engineSchematicTest)
+console.log(part2(engineSchematicTest))
 
-console.log(
-  'adyacentsNumbers',
-  findAdyacentsNumbers(engineSchematicTest, asthericCoordinates)
-)
+// La solución de la parte 2 la encontré en https://www.youtube.com/watch?v=dok4MZ_cEyk&ab_channel=frontendcoder
