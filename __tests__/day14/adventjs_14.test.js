97a78d83c9e7083c1790c72744c48aaf98261d47
Juan Carlos Galue <juancgalue@icloud.com>
2023-12-30 01:36:11 -0300

    Otro test secreto

--- __tests__/day14/adventjs_14.test.js
+++ __tests__/day14/adventjs_14.test.js
@@ -7,4 +7,5 @@ test('Reto 14: evita la alarma', () => {
   expect(maxGifts([4, 1, 1, 4, 2, 1])).toBe(9)
   expect(maxGifts([1, 3, 1, 3, 100])).toBe(103)
   expect(maxGifts([1, 2, 3, 1])).toBe(4)
+  expect(maxGifts([2, 7, 9, 3, 1])).toBe(12)
 })
